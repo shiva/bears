1f4dd11354df46dc517318d4a4daceaf48a55d9e
Shiva <shiv@shiv.me>
2015-02-22 13:36:12 -0800

    Bear model with Create and ListAll support

---
+++ server.js
@@ -0,0 +1,89 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var Bear       = require('./app/models/bear');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://bears:bears@ds031617.mongolab.com:31617/bears');
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+// create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Bear created!' });
+        });
+
+    })
+
+// get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
+            if (err) {
+                res.send(err);
+            } 
+
+            res.json(bears);
+        });
+    });
+
+// on routes that end in bears/:bear_id
+router.route('/bears/:bear_id')
+    // get the bear with specified id
+    .get(function(req, res){
+        console.log(req.param.bear_id)
+        Bear.findById(req.param.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+            console.log(bear)
+        });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
